21c1f15f2e462d6c5bb08bf274f0ddf20b35bcfa
“airrinakano” <airinakano0212@gmail.com>
2022-04-28 23:41:32 +0900

    カレンダーライブラリーを使うもので表示

--- js/main.js
+++ js/main.js
@@ -1,36 +1,31 @@
 //トグルボタンが押されたときに、クラスを付与
-function addClass(){
-   const nav=document.getElementsByClassName('nav');
-    const nav_toggle=document.getElementsByClassName('nav_toggle');
-    nav[0].classList.toggle('show');
-    nav_toggle[0].classList.toggle('show');
- 
-    const header=document.getElementsByClassName('header');
-    
-    header[0].classList.toggle('active');
+function addClass() {
+  const nav = document.getElementsByClassName("nav");
+  const nav_toggle = document.getElementsByClassName("nav_toggle");
+  nav[0].classList.toggle("show");
+  nav_toggle[0].classList.toggle("show");
+
+  const header = document.getElementsByClassName("header");
+
+  header[0].classList.toggle("active");
 }
 
+currentNum = 0;
 
-currentNum=0;
+function add() {
+  const img = document.getElementById("img");
+  const photes = ["img/8.png", "img/６.png"];
 
-function add(){
-    const img=document.getElementById('img');
-    const photes=['img/8.png','img/６.png'];
-
-    img.src=photes[currentNum];
-    currentNum++;
+  img.src = photes[currentNum];
+  currentNum++;
 }
-function back(){
+function back() {}
 
-}
-
-$(document).ready(function() {
-
-    $(".slide").animate({left: "+=840"}, 2000);
-    $(".slide").animate({left: "+=1680"}, 2000);
-   
-   
+$(document).ready(function () {
+  $(".slide").animate({ left: "+=840" }, 2000);
+  $(".slide").animate({ left: "+=1680" }, 2000);
 });
 
+//カレンダー表示
 
 // 'img/５.png','img/９.png'
